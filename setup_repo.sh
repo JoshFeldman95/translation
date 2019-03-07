9500fb83e42c8979a2f1bb7235ed5a91c1119144
Josh Feldman <joshfeldman95@gmail.com>
2019-03-06 21:34:04 -0500

    added bleu

--- setup_repo.sh
+++ setup_repo.sh
@@ -2,3 +2,4 @@ pip3 install -q torch torchtext spacy opt_einsum
 pip3 install -qU git+https://github.com/harvardnlp/namedtensor
 python3 -m spacy download en
 python3 -m spacy download de
+curl -O https://raw.githubusercontent.com/harvard-ml-courses/cs287-s18/master/HW3/source_test.txt
